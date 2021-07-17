131e7e80315dd6774772a0e3cabb3e3c21fecfff
ShreyaAdhya <76802799+ShreyaAdhya@users.noreply.github.com>
2021-07-17 10:57:54 +0530

    Add files via upload

---
+++ CodonPackage/calc/Avg.java
@@ -0,0 +1,10 @@
+package calc;
+
+public class Avg
+{
+	public void avg(double a, double b)
+	{
+		System.out.println("Average of "+a+" and "+b+" = "+((a+b)/2));
+	}	
+	
+}
---
+++ CodonPackage/calc/Sub.java
@@ -0,0 +1,18 @@
+package calc;
+
+public class Sub
+{
+	public void subtract(double a, double b)
+	{
+	  if(a>b)
+		System.out.println(a+"-"+b+" = "+(a-b));
+	  else
+		System.out.println(b+"-"+a+" = "+(b-a));  
+	}	
+	
+    /*public static void main(String[] args)
+    {
+		System.out.println("Subtracting ...");
+		
+    }*/
+}
